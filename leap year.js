c2a5f1d2c1e6767197e392740fa24547b78b1d8b
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-12 10:44:47 +0600

    more easy calculation added

--- leap year.js
+++ leap year.js
@@ -1,13 +1,16 @@
-function leapYear (year){
-    if (year % 4 === 0 && year % 100 !== 0){
+function leapYear(year) {
+    if (year % 4 === 0 && year % 100 !== 0) {
         return true
     }
-    else if(year % 400 === 0 && year % 100 === 0){
+    else if (year % 400 === 0) {
         return true;
     }
     return 'not a leap year'
 }
 
-
-const count = leapYear(2024)
+// indirectly value assign 
+const count = leapYear(1900)
 console.log(count)
+
+// Directly value assign
+// console.log(leapYear(1800))
